bdbffaabfd559fce5c1cf6101421844659a4dd13
Kelly Lee <amorfati38@gmail.com>
2022-04-26 21:31:47 -0600

    added about me

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import avatarImg from '../../assets/images/Avatar.png';
+
+function About() {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <img src={avatarImg} alt='avatar'/>
+            <p>
+                Hi, my name is Hyunkyoung Lee. I graduated in psychology for my bachelor degree, but after I got married with an enthusiastic back-end engineer, I became to be interested in coding. 
+                I've been really loving coding and developing websites because it is a creative art of creating different forms of expression by utilizing computer languages. 
+                I'm sharpening my coding skills for HTML, CSS, JavaScript, React, Node.js, MySQL, and MongoDB.
+                Little more personal information about me beside coding, I'm a mom of two adorable girls running all over the place. My favorite dessert is chocolate ice cream and my family loves to go to camping and fishing.
+            </p>
+        </section>
+    );
+}
+
+export default About;
